244c897f64d2d91458d7d9f1f8c28f5d65346ffb
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2024-05-11 16:47:43 -0500

    Update init.sh

--- template/init.sh
+++ template/init.sh
@@ -1,3 +1,5 @@
+#from: https://docs.conan.io/2/tutorial/consuming_packages/build_simple_cmake_project.html
+
 # Run once To setup a system
 # let Conan try to guess the profile, based on the current operating system and installed tools:
 conan profile detect --force

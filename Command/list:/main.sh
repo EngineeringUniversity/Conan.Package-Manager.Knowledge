b4f45f48f67b3949bce8f11ad4a7d118040337e8
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2024-05-11 16:57:53 -0500

    Update main.sh

--- Command/list:/main.sh
+++ Command/list:/main.sh
@@ -1,3 +1,6 @@
+# Tutorial, from: https://docs.conan.io/2/tutorial/consuming_packages/build_simple_cmake_project.html
 conan profile detect --force
 
 conan profile path default
+
+conan install . --output-folder=build --build=missing
